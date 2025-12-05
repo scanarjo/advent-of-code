09d076fed71aabb15add7fd6f57c2de14a666e82
Scott Arden-Jones <4737932+scanarjo@users.noreply.github.com>
2025-12-05 18:56:39 +0000

    2025-05: Solution for part 1

--- 2025/day/05/index.ts
+++ 2025/day/05/index.ts
@@ -1,1 +1,6 @@
-console.log("Hello via Bun!");
+import { readLinesFromFile } from 'utils';
+import { countFreshIngredients } from './ingredient-checker';
+
+const input = await readLinesFromFile('input.txt');
+
+console.log('Answer for part 1:', countFreshIngredients(input));
--- 2025/day/05/ingredient-checker.ts
+++ 2025/day/05/ingredient-checker.ts
@@ -1,9 +1,25 @@
+import { parseDenaryInt } from 'utils';
+
+function isInRange(min: number, max: number, n: number) {
+  return n >= min && n < max;
+}
+
 export function countFreshIngredients(sampleData: string[]): number {
   const splitIndex = sampleData.findIndex((string) => string === '');
 
-  const freshRanges = sampleData.slice(0, splitIndex);
+  const freshRanges = sampleData
+    .slice(0, splitIndex)
+    .map((range) => range.split('-'));
 
-  const ingredientIds = sampleData.slice(splitIndex + 1);
+  let ingredientIds = sampleData.slice(splitIndex + 1).map(parseDenaryInt);
 
-  return ingredientIds.length;
+  const totalIngredients = ingredientIds.length;
+  for (const range of freshRanges) {
+    const min = parseDenaryInt(range[0]!);
+    const max = parseDenaryInt(range[1]!);
+
+    ingredientIds = ingredientIds.filter((id) => !isInRange(min, max + 1, id));
+  }
+
+  return totalIngredients - ingredientIds.length;
 }
