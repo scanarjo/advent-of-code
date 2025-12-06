e2210881e1c43fbc15b415ea28695975f41d7f7a
Scott Arden-Jones <4737932+scanarjo@users.noreply.github.com>
2025-12-06 11:09:38 +0000

    2025-05: Solution to part 2

--- 2025/day/05/ingredient-checker.spec.ts
+++ 2025/day/05/ingredient-checker.spec.ts
@@ -4,9 +4,11 @@ import { readLinesFromFile } from 'utils';
 import {
   countAllPossibleFreshIngredients,
   countFreshIngredients,
+  mergeRanges,
 } from './ingredient-checker';
 
 const sampleData = await readLinesFromFile(__dirname + '/sample.txt');
+const inputDate = await readLinesFromFile(__dirname + '/input.txt');
 
 describe('countFreshIngredients', () => {
   it('should count the correct number of fresh ingredients in the sample data', () => {
@@ -22,5 +24,21 @@ describe('countFreshIngredients', () => {
 describe('countAllPossibleFreshIngredients', () => {
   it('should count the correct number of possible fresh ingredients in the sample data', () => {
     expect(countAllPossibleFreshIngredients(sampleData)).toBe(14);
+  });
+
+  it('should count the correct number of possible fresh ingredients in the input data', () => {
+    expect(countAllPossibleFreshIngredients(inputDate)).toBe(352340558684863);
+  });
+});
+
+describe('mergeRanges', () => {
+  it('should merge overlapping ranges correctly', () => {
+    expect(mergeRanges([5, 10], [7, 15])).toEqual([5, 15]);
+    expect(mergeRanges([7, 15], [5, 10])).toEqual([5, 15]);
+  });
+
+  it('should return undefined if ranges do not overlap', () => {
+    expect(mergeRanges([0, 5], [6, 10])).toBe(undefined);
+    expect(mergeRanges([6, 10], [0, 5])).toBe(undefined);
   });
 });
--- 2025/day/05/ingredient-checker.ts
+++ 2025/day/05/ingredient-checker.ts
@@ -1,8 +1,6 @@
-import { parseDenaryInt } from 'utils';
+import { isInRange, parseDenaryInt } from 'utils';
 
-function isInRange(min: number, max: number, n: number) {
-  return n >= min && n < max;
-}
+type Range = [bottom: number, top: number];
 
 export function countFreshIngredients(sampleData: string[]): number {
   const splitIndex = sampleData.findIndex((string) => string === '');
@@ -23,30 +21,64 @@ export function countFreshIngredients(sampleData: string[]): number {
 
   return totalIngredients - ingredientIds.length;
 }
+
+function parseRange(range: string): Range | undefined {
+  const [bottom, top] = range.split('-');
+
+  if (!bottom || !top) return;
+
+  return [parseDenaryInt(bottom), parseDenaryInt(top)];
+}
+
 export function countAllPossibleFreshIngredients(sampleData: string[]): number {
   const splitIndex = sampleData.findIndex((string) => string === '');
 
   const freshRanges = sampleData
     .slice(0, splitIndex)
-    .map((range) => range.split('-'))
-    .map((range) => range.map(parseDenaryInt))
+    .map(parseRange)
+    .filter((range) => range !== undefined)
     .sort((rangeA, rangeB) => rangeA[0]! - rangeB[0]!);
 
-  const min = freshRanges[0]![0]!;
+  let flattendRanges = <Range[]>[];
+  while (freshRanges.length !== 0) {
+    let range = freshRanges.shift()!;
 
-  const adjustedRanges = freshRanges.map((range) => range.map((n) => n - min));
+    let nextRange = freshRanges.shift();
+    while (nextRange !== undefined) {
+      const mergedRange = mergeRanges(range, nextRange);
 
-  console.log(adjustedRanges);
+      if (!mergedRange) {
+        flattendRanges.push(range);
+        range = nextRange;
+      } else {
+        range = mergedRange;
+      }
 
-  const freshIds = new Set<number>();
-  for (const range of freshRanges) {
-    const min = range[0]!;
-    const max = range[1]!;
-
-    for (let n = min; n < max + 1; n++) {
-      freshIds.add(n);
+      nextRange = freshRanges.shift();
     }
+
+    flattendRanges.push(range);
   }
 
-  return freshIds.size;
+  return flattendRanges.reduce((total, [bottom, top]) => {
+    return total + top - bottom + 1;
+  }, 0);
+}
+
+function isOverlapping(
+  [bottomA, topA]: Range,
+  [bottomB, topB]: Range
+): boolean {
+  return (
+    isInRange(bottomA, topA + 1, bottomB) || isInRange(bottomA, topA + 1, topB)
+  );
+}
+
+export function mergeRanges(rangeA: Range, rangeB: Range): Range | undefined {
+  if (!isOverlapping(rangeA, rangeB)) return;
+
+  const bottom = Math.min(...[rangeA, rangeB].map(([bottom]) => bottom));
+  const top = Math.max(...[rangeA, rangeB].map(([, top]) => top));
+
+  return [bottom, top];
 }
